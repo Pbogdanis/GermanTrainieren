53b5c42b7cffa12832f29d5c6405fc170540a9ea
Pbogdanis <pbogdanis@hotmail.com>
2020-03-31 20:26:06 +0300

    Refactored Articles -- Added Snackbar

--- app/src/main/java/com/gerproject/germantrainieren/Articles.java
+++ app/src/main/java/com/gerproject/germantrainieren/Articles.java
@@ -8,6 +8,8 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
+
 import java.util.List;
 
 import Models.ArticlesModel;
@@ -25,7 +27,6 @@ public class Articles extends AppCompatActivity implements View.OnClickListener 
     private List<ArticlesModel> _allArticles;
     private Integer _random_index;
     private String _btnPressedTxt;
-    private Toast _toastMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,40 +63,6 @@ public class Articles extends AppCompatActivity implements View.OnClickListener 
                 setContentView(R.layout.failure_layout);
             }
         });
-
-
-
-
-        /*ArticlesModel newSingular = new ArticlesModel();
-        newSingular.setSingular("testFromAndroid01");
-        newSingular.setArticle("testFromAndroid02");
-        Call<List<ArticlesModel>> call = jsonPlaceHolderApi.InsertSingular(newSingular);
-        call.enqueue(new Callback<List<ArticlesModel>>() {
-            @Override
-            public void onResponse(Call<List<ArticlesModel>> call, Response<List<ArticlesModel>> response) {
-                if(!response.isSuccessful()){
-                    textViewResult.setText("Code: " + response.code() + response.message());
-                    return;
-                }
-                String result = String.valueOf(response.code());
-                textViewResult.setText(result);
-
-            }
-
-            @Override
-            public void onFailure(Call<List<ArticlesModel>> call, Throwable t) {
-                textViewResult.setText(t.getMessage());
-            }
-        });*/
-
-        /*cpArticles = new ArrayList<String>(articles);
-        cpSingular = new ArrayList<String>(singular);
-
-        _question_txt = findViewById(R.id.question_txt);
-        _answer_txt = findViewById(R.id.answer_txt);
-        _random_index = getRandomNumberInRange(0, cpArticles.size() - 1);
-        _next_article = "The article of " + cpSingular.get(_random_index) + " is : ";
-        _question_txt.setText(_next_article);*/
     }
 
     @Override
@@ -115,9 +82,9 @@ public class Articles extends AppCompatActivity implements View.OnClickListener 
 
 
         if( _allArticles.get(_random_index).getArticle().equalsIgnoreCase(_btnPressedTxt) ){
-            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
+            Snackbar.make(v, getString(R.string.correct), Snackbar.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(this, "Wrong! The correct article is " + _allArticles.get(_random_index).getArticle(), Toast.LENGTH_SHORT).show();
+            Snackbar.make(v, getString(R.string.wrong)+ " " + _allArticles.get(_random_index).getArticle(), Snackbar.LENGTH_SHORT).show();
         }
 
         //Remove word from list//
@@ -131,47 +98,10 @@ public class Articles extends AppCompatActivity implements View.OnClickListener 
             _wordFromList.setText(_allArticles.get(_random_index).getSingular());
         } else {
             //Close activity and show MainActivity
-            _toastMsg = Toast.makeText(this, "The quiz is over!", Toast.LENGTH_SHORT);
-            _toastMsg.setGravity(Gravity.TOP| Gravity.CENTER, 0, 0);
-            _toastMsg.show();
+            Snackbar.make(v, getString(R.string.over), Snackbar.LENGTH_SHORT).show();
 
             finish();
         }
-
-        /*if(!cpArticles.isEmpty()){
-            if (_answer_txt.getText().toString().isEmpty()){
-                Toast.makeText(this, "Please select an answer", Toast.LENGTH_SHORT).show();
-            } else {
-
-                if (cpArticles.get(_random_index).equalsIgnoreCase(_answer_txt.getText().toString())) {
-                    Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(this, "Wrong! The correct article is " + cpArticles.get(_random_index), Toast.LENGTH_SHORT).show();
-                }
-
-                //Remove word from the lists
-                cpArticles.remove(_random_index);
-                cpSingular.remove(_random_index);
-
-                //Update view
-                if(!cpArticles.isEmpty()){
-                    //Generate new random index
-                    _random_index = getRandomNumberInRange(0, cpArticles.size() - 1);
-                    //Update view
-                    _next_article = "The article of " + cpSingular.get(_random_index) + " is : ";
-                    _question_txt.setText(_next_article);
-                    _answer_txt.setText("");
-                } else {
-                   //Close activity and show MainActivity
-                    toastMsg = Toast.makeText(this, "The quiz is over!", Toast.LENGTH_SHORT);
-                    toastMsg.setGravity(Gravity.TOP| Gravity.CENTER, 0, 0);
-                    toastMsg.show();
-
-                    finish();
-                }
-
-            }
-        }*/
     }
 
 
